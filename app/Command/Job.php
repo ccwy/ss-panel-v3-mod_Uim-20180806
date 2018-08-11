6c0c4ea36a8da8b7dab5fa8b33d33d03e2c0ae47
ccwy <ccwy@users.noreply.github.com>
2018-08-11 20:08:52 +0800

    20180811

--- app/Command/Job.php
+++ app/Command/Job.php
@@ -42,9 +42,7 @@ class Job
     {
         $nodes = Node::all();
         foreach ($nodes as $node) {
-			//$ip = gethostbyname($node->server);
-		//	if ($node->node_ip != $ip && $node->sort != '9') {        
-            if ($node->sort==0 || $node->sort==10) {  
+            if ($node->sort==0 || $node->sort==10) {
                 $ip=gethostbyname($node->server);
                 $node->node_ip=$ip;
                 $node->save();
