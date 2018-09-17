a9ffc614086702fdf18a51acea22ffaa5e3c3f9a
ccwy <ccwy@users.noreply.github.com>
2018-09-17 23:42:04 +0800

    52

--- app/Command/Job.php
+++ app/Command/Job.php
@@ -870,7 +870,7 @@ class Job
 			
 			
 		      	//等级到期3天后提醒用户续费	
-		     	if($user->class ==0 && time() - strtotime($user->class_expire) >= 3*86400 && $user->tixing != 3 && $user->tixing != 4)
+		     	if($user->class ==0 && time() - strtotime($user->class_expire) >= 3*86400 && $user->tixing == 2)
 			    {				
 					
 					$subject = Config::get('appName')."-您的账号等级已过期 3 天了";
@@ -904,7 +904,7 @@ class Job
 			
 			
 			//等级到期删除用户前7天最后提醒
-			if($user->class ==0 && time() - strtotime($user->class_expire) >= 7*86400 && $user->tixing !=3 && $user->tixing != 4)
+			if($user->class ==0 && time() - strtotime($user->class_expire) >= 7*86400 && $user->tixing ==3)
 			{				
 					
 					$subject = Config::get('appName')."-您的账号已过期7天以上，账号即将被删除";
