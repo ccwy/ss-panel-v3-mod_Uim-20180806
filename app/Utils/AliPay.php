3f019df99c028e6a4919eacf6db58d0e0d5ea412
ccwy <ccwy@users.noreply.github.com>
2018-09-29 20:08:12 +0800

    63

--- app/Utils/AliPay.php
+++ app/Utils/AliPay.php
@@ -263,7 +263,7 @@ class AliPay
 //                        return $item['outTradeNo'];
 //                    }
                     if ($item['signProduct'] == '转账收款码' && $item['accountType'] == '交易' &&
-                        strtotime($item['tradeTime']) < $time && strtotime($item['tradeTime']) > $time-120 && $item['tradeAmount'] == $fee) {
+                        strtotime($item['tradeTime']) < $time && strtotime($item['tradeTime']) > $time-180 && $item['tradeAmount'] == $fee) {
                         if (!Paylist::where('tradeno', $item['tradeNo'])->first())
                             return $item['tradeNo'];
                     }
