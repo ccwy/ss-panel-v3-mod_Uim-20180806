5369330025c44bdd6691d4d84dac72efd7b202e3
ccwy <ccwy@users.noreply.github.com>
2018-10-14 16:30:08 +0800

    5

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ $countryCode = geoip_country_code_by_addr($geoData, $_SERVER['REMOTE_ADDR']);
 geoip_close($geoData);  
 global $countryCode;   
 if($countryCode == 'CN') {
-if (substr($_SERVER["REQUEST_URI"],0,9) === '/error_ip' or substr($_SERVER["REQUEST_URI"],0,5) === '/cous'  or substr($_SERVER["REQUEST_URI"],0,17) === '/codepay_callback' or substr($_SERVER["REQUEST_URI"], 0, 60) === '/link' or substr($_SERVER["REQUEST_URI"], 0, 30) === '/sssss' or substr($_SERVER["REQUEST_URI"], 0, 15) === '/cpts123' or substr($_SERVER["REQUEST_URI"], 0, 21) === '/codepay_pay_callback' or substr($_SERVER["REQUEST_URI"], 0, 16) === '/codepay_pay_callback'){	
+if (substr($_SERVER["REQUEST_URI"],0,9) === '/error_ip' or substr($_SERVER["REQUEST_URI"],0,5) === '/cous'  or substr($_SERVER["REQUEST_URI"],0,17) === '/codepay_callback' or substr($_SERVER["REQUEST_URI"], 0, 5) === '/link' or substr($_SERVER["REQUEST_URI"], 0, 30) === '/sssss' or substr($_SERVER["REQUEST_URI"], 0, 15) === '/cpts123' or substr($_SERVER["REQUEST_URI"], 0, 21) === '/codepay_pay_callback' or substr($_SERVER["REQUEST_URI"], 0, 16) === '/codepay_pay_callback'){	
 //  PUBLIC_PATH
 define('PUBLIC_PATH', __DIR__);
 // Bootstrap
